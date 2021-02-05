06ef8370dd1ef86c5e183fc277ae7f5df5bb7165
HemalathaRao <hemalatharao936@gmail.com>
2021-02-05 20:13:31 +0530

    Initial Commit

--- app/src/main/java/com/example/market/Additems.java
+++ app/src/main/java/com/example/market/Additems.java
@@ -1,14 +1,27 @@
 package com.example.market;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Additems extends AppCompatActivity {
+    Button Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additems);
+        Back=(Button) findViewById(R.id.button2);
+        Back.setOnClickListener(new View.OnClickListener() {
+            //            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Additems.this,Homepage.class);//move to next page
+                startActivity(intent);
+            }
+        });
     }
 }
